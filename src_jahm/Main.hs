011027ad8cc491a1862991ba5cff9cc1dcc99acd
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-11-08 10:09:22 +0900

    Test code for jahm. (cont.)

--- src_jahm/Main.hs
+++ src_jahm/Main.hs
@@ -1,9 +1,10 @@
 {- Jahm: Jhc and Ajhc Haskell libraries Manager -}
 module Main where
 import Data.Maybe
+import Data.List
 import Control.Monad
 import System.Environment (getArgs)
-import System.Directory (getCurrentDirectory, findExecutable)
+import System.Directory
 import System.FilePath ((</>))
 import System.Exit
 import Network.URI (parseURI)
@@ -11,6 +12,8 @@ import Distribution.Client.HttpUtils (downloadURI)
 import Distribution.Verbosity (verbose)
 import GenUtil (iocatch)
 import Support.CompatMingw32 (systemCompat)
+import Support.Cabal
+import Version.Version
 
 main :: IO ()
 main = getArgs >>= mainWorker
@@ -26,5 +29,17 @@ mainWorker ("make":a) = do
   mayGmake <- findExecutable "gmake"
   e <- systemCompat $ fromMaybe "make" mayGmake ++ " " ++ unwords a
   when (e /= ExitSuccess) $ exitWith e
+
+mainWorker ("makefile":_) = do -- xxx for test
+  -- create dir
+  home <- getHomeDirectory
+  let f ' ' = '_'
+      f a   = a
+      basedir  = home </> ".ajhc" </> "jahm" </> fmap f (delete '(' . delete ')' $ versionSimple)
+      libdir   = basedir </> "lib"
+      builddir = basedir </> "build"
+  createDirectoryIfMissing True libdir
+  createDirectoryIfMissing True builddir
+  -- find cabal and yaml files
 
 mainWorker _ = print "help"
